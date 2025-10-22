5639558602eca632d07f7347caa46abc7f653fea
Amadeusz Kobos <129185201+AmadeuszKobos@users.noreply.github.com>
2025-10-23 00:33:44 +0200

    Stabilize Venues API test seeding

--- EventTicketingAPI/Program.cs
+++ EventTicketingAPI/Program.cs
@@ -1,11 +1,26 @@
 using EventTicketingAPI.DI;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<AppDbContext>((serviceProvider, options) =>
+{
+  var env = serviceProvider.GetRequiredService<IHostEnvironment>();
+  if (env.IsEnvironment("Testing"))
+  {
+    var configuration = serviceProvider.GetRequiredService<IConfiguration>();
+    var databaseName = configuration.GetValue<string>("Testing:InMemoryDatabaseName") ?? "EventTicketingApiTests";
+    options.UseInMemoryDatabase(databaseName);
+  }
+  else
+  {
+    var configuration = serviceProvider.GetRequiredService<IConfiguration>();
+    options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"));
+  }
+});
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -28,3 +43,5 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+public partial class Program { }
